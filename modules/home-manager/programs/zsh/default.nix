2be2510ffcbfda967bb2127492fafb33156d98b1
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2025-07-19 00:05:48 +0530

    Add few more zsh options

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -8,6 +8,8 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable  = true;
     shellAliases = {
       lg = "lazygit";
 
